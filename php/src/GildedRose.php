e15e5ec189f96058d2f7eca6c88fc6b6d4bf2e3b
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-04-26 21:03:07 +0300

    Change updateQuality logic

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -7,7 +7,7 @@ namespace GildedRose;
 final class GildedRose
 {
     /**
-     * @var Item[]
+     * @var Item[]  array of created Item class products
      */
     private $items;
 
@@ -19,51 +19,38 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        --$item->quality;
-                    }
-                }
-            } else {
+
+            if ($item->name == 'Sulfuras, Hand of Ragnaros') continue;
+            
+            if ($this->isTheOlderTheBetter($item)) {
                 if ($item->quality < 50) {
                     ++$item->quality;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                ++$item->quality;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                ++$item->quality;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                --$item->sell_in;
-            }
-
-            if ($item->sell_in < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                --$item->quality;
-                            }
-                        }
+                    
+                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($item->sell_in > 5 && $item->sell_in <= 10) $item->quality += 1;
+                        else if ($item->sell_in <= 5 && $item->sell_in > 0) $item->quality += 2;
                     } else {
-                        $item->quality -= $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        ++$item->quality;
+                        if ($item->sell_in <= 0) ++$item->quality;
                     }
                 }
+                if ($item->quality > 50 && $item->sell_in > 0) $item->quality = 50;
+                else if ($item->sell_in <= 0 && $item->name == 'Backstage passes to a TAFKAL80ETC concert') $item->quality = 0;
+
+            } else {
+                if ($item->sell_in > 0 && $item->quality > 0) --$item->quality;
+                if ($item->sell_in <= 0 && $item->quality > 1) $item->quality -= 2;
+                else if ($item->sell_in <= 0 && $item->quality == 1) --$item->quality;
             }
+
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') --$item->sell_in;
         }
+    }
+
+    public function isTheOlderTheBetter($item)
+    {
+        $reverse = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'];
+        $result = in_array($item->name, $reverse) ? true : false;
+
+        return $result;
     }
 }
