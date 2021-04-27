475103f89e406b64e2496b3e7cab23f3399359a4
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-04-27 08:45:21 +0300

    Update GildedRose updateQuality method

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use Inventory\Backstage\Backstage;
+use Inventory\Brie\Brie;
+use Inventory\Normal\Normal;
+use Inventory\Sulfuras\Sulfuras;
+
 final class GildedRose
 {
     /**
@@ -19,38 +24,25 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-
-            if ($item->name == 'Sulfuras, Hand of Ragnaros') continue;
-            
-            if ($this->isTheOlderTheBetter($item)) {
-                if ($item->quality < 50) {
-                    ++$item->quality;
-                    
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in > 5 && $item->sell_in <= 10) $item->quality += 1;
-                        else if ($item->sell_in <= 5 && $item->sell_in > 0) $item->quality += 2;
-                    } else {
-                        if ($item->sell_in <= 0) ++$item->quality;
-                    }
-                }
-                if ($item->quality > 50 && $item->sell_in > 0) $item->quality = 50;
-                else if ($item->sell_in <= 0 && $item->name == 'Backstage passes to a TAFKAL80ETC concert') $item->quality = 0;
-
-            } else {
-                if ($item->sell_in > 0 && $item->quality > 0) --$item->quality;
-                if ($item->sell_in <= 0 && $item->quality > 1) $item->quality -= 2;
-                else if ($item->sell_in <= 0 && $item->quality == 1) --$item->quality;
+            switch ($item->name) {
+                case 'Sulfuras, Hand of Ragnaros':
+                    Sulfuras::create($item);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    Backstage::create($item);
+                    break;
+                case 'Aged Brie':
+                    Brie::create($item);
+                    break;
+                // // case 'Conjured Mana Cake':
+                //     // ConjuredManaCake::create();
+                default:
+                    Normal::create($item);
             }
 
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') --$item->sell_in;
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
+                --$item->sell_in;
+            }
         }
-    }
-
-    public function isTheOlderTheBetter($item)
-    {
-        $reverse = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'];
-        $result = in_array($item->name, $reverse) ? true : false;
-
-        return $result;
     }
 }
